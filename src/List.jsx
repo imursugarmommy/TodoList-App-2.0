fc70be6de41bbee7b4dd260683080e1ca70c2697
Levi <139384471+imursugarmommy@users.noreply.github.com>
2023-10-18 22:12:52 +0200

    Did a modal with collapsises
    Missclicked last time, that's the real one lol 
    When you click the add list button there will be a modal wher you can modify that list, if you don't want to its gonna have default settings ofc.
    Want to add an after confirmation edit, so they can change whatever.

--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function List({ taskCount }) {
+export default function List({ taskCount, deleteList, list }) {
   return (
     <li className="list-item no-active">
-      <p className="tasks">{taskCount ? taskCount : 0} Tasks</p>
+      <div className="box flex">
+        <p className="tasks">{taskCount ? taskCount : 0} Tasks</p>
+        <i className="fa-solid fa-trash"></i>
+      </div>
       <h3 className="list-name">Todo List</h3>
       <div className="line">
         <div className="progress"></div>
